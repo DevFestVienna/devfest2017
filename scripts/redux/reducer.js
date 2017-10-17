b1c0023cf4cd40fe3e0ee630b82f015bdd153d44
marcedem <marcsea2000@gmail.com>
2017-10-17 02:55:31 +0200

    Codebase refactoring - implementation of schedule and speakers page

--- scripts/redux/reducer.js
+++ scripts/redux/reducer.js
@@ -2,25 +2,31 @@ const uiReducer = (state = initialState.ui, action) => {
   switch (action.type) {
     case TOGGLE_DRAWER:
       return Object.assign({}, state, {
-        isDrawerOpened: action.value || !state.isDrawerOpened
+        isDrawerOpened: action.value || !state.isDrawerOpened,
       });
     case TOGGLE_VIDEO_DIALOG:
-      return Object.assign({}, state,  {
-        videoDialog: Object.assign({}, state.videoDialog, action.value)
+      return Object.assign({}, state, {
+        videoDialog: Object.assign({}, state.videoDialog, action.value),
       });
     case SET_VIEWPORT_SIZE:
       return Object.assign({}, state, {
-        viewport: Object.assign({}, state.viewport, action.value)
+        viewport: Object.assign({}, state.viewport, action.value),
       });
     default:
       return state;
   }
 };
 
-const routeReducer = (state = initialState.route, action) => {
+const routingReducer = (state = initialState.routing, action) => {
   switch (action.type) {
     case SET_ROUTE:
-      return action.route;
+      return Object.assign({}, state, {
+        route: action.route,
+      });
+    case SET_SUB_ROUTE:
+      return Object.assign({}, state, {
+        subRoute: action.subRoute,
+      });
     default:
       return state;
   }
@@ -38,10 +44,11 @@ const heroReducer = (state = initialState.hero, action) => {
 const dialogsReducer = (state = initialState.dialogs, action) => {
   switch (action.type) {
     case OPEN_DIALOG:
+    case SET_DIALOG_DATA:
       return Object.assign({}, state, action.dialog);
     case CLOSE_DIALOG:
       return Object.assign({}, state, {
-        [action.dialogName]: initialState.dialogs[action.dialogName]
+        [action.dialogName]: initialState.dialogs[action.dialogName],
       });
     default:
       return state;
@@ -87,7 +94,34 @@ const blogReducer = (state = initialState.blog, action) => {
 const speakersReducer = (state = initialState.speakers, action) => {
   switch (action.type) {
     case FETCH_SPEAKERS_LIST:
+    case UPDATE_SPEAKERS:
       return action.list;
+    default:
+      return state;
+  }
+};
+
+const sessionsReducer = (state = initialState.sessions, action) => {
+  switch (action.type) {
+    case FETCH_SESSIONS_LIST:
+    case UPDATE_SESSIONS:
+      return Object.assign({}, state, {
+        list: action.list,
+      });
+    case FETCH_USER_FEATURED_SESSIONS:
+    case SET_USER_FEATURED_SESSIONS:
+      return Object.assign({}, state, {
+        featured: action.featuredSessions || {},
+      });
+    default:
+      return state;
+  }
+};
+
+const scheduleReducer = (state = initialState.schedule, action) => {
+  switch (action.type) {
+    case FETCH_SCHEDULE:
+      return action.data;
     default:
       return state;
   }
@@ -106,6 +140,48 @@ const teamReducer = (state = initialState.team, action) => {
   switch (action.type) {
     case FETCH_TEAM:
       return action.team;
+    default:
+      return state;
+  }
+};
+
+const userReducer = (state = initialState.user, action) => {
+  switch (action.type) {
+    case SIGN_IN:
+    case SIGN_OUT:
+      return action.user;
+    default:
+      return state;
+  }
+};
+
+const subscribeReducer = (state = initialState.subscribed, action) => {
+  switch (action.type) {
+    case SUBSCRIBE:
+      return action.subscribed;
+    default:
+      return state;
+  }
+};
+
+const toastReducer = (state = initialState.toast, action) => {
+  switch (action.type) {
+    case SHOW_TOAST:
+      return action.toast;
+    case HIDE_TOAST:
+      return initialState.toast;
+    default:
+      return state;
+  }
+};
+
+const notificationsReducer = (state = initialState.notifications, action) => {
+  switch (action.type) {
+    case UPDATE_NOTIFICATIONS_STATUS:
+      return Object.assign({}, state, {
+        status: action.status,
+        token: action.token,
+      });
     default:
       return state;
   }
